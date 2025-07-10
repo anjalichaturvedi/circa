1b28e970ae78a3d3a021a19a08de85f7fe32959c
Anjali Chaturvedi <anjaliichaturvedi@gmail.com>
2025-07-11 01:49:04 +0530

    Initial Commit

---
+++ circaTests/circaTests.swift
@@ -0,0 +1,17 @@
+//
+//  circaTests.swift
+//  circaTests
+//
+//  Created by Anjali Chaturvedi on 11/07/25.
+//
+
+import Testing
+@testable import circa
+
+struct circaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

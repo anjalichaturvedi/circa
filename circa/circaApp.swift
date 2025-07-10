b46e7cd35cd25e9544439afcfe676acbe0335834
Anjali Chaturvedi <contact@anjalichaturvedi.tech>
2025-07-11 01:49:04 +0530

    Initial Commit

---
+++ circa/circaApp.swift
@@ -0,0 +1,17 @@
+//
+//  circaApp.swift
+//  circa
+//
+//  Created by Anjali Chaturvedi on 11/07/25.
+//
+
+import SwiftUI
+
+@main
+struct circaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
